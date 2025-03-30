71c441f230b049da13587d23301f9f331fd7b616
Nsrow2510 <navneetsrow@gmail.com>
2025-03-30 12:46:23 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 void fibonacciSeries(int n) {
-    int n;
-    scanf("%d", &n);
+    int x;
+    scanf("%d", &x);
     int a = 0, b = 1;
-    for(int i =1; i<=n; i++) {
+    for(int i =1; i<=x; i++) {
         int next = a+b;
         printf("%d", a);
         a = b;
