51cfec10ce6646f7b6383098f2893edd65fb58a4
Nsrow2510 <navneetsrow@gmail.com>
2025-04-09 20:17:56 +0530

    Solution to Caesar Cipher Encryption

---
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -0,0 +1,14 @@
+void caeserCipher(char message[], int shift, char encrypted[]) {
+    for (int i = 0; message[i] != '\0'; i++) {
+        char ch = message[i];
+
+        if(ch >= 'a' && ch <= 'z') {
+            encrypted[i] = ((ch - 'a' + shift) % 26) + 'a';
+        } else if (ch >= 'A' && ch <= 'Z') { 
+            encrypted[i] = ((ch - 'A' + shift)%26) + 'A';
+        }
+    }
+
+    printf("%s", encrypted);
+
+}
